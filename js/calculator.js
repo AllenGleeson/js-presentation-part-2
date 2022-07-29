808a6b088161821df4d45e9cc6e5b22819a9b2be
AllenGleeson <allen_gleeson@hotmail.com>
2022-07-29 03:05:08 +0100

    Added some styling, a getter and setter for the display to trim down some repetition and did some more work on the functions.

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,15 @@
+let firstNum = false;
+let secondNum = false;
+let operator;
+
+function getDisplay(){
+	return document.getElementById("display").innerText;
+}
+
+function printDisplay(num){
+	document.getElementById("display").innerText=num;
+}
+
 function operate(operator, firstNum, secondNum ) {
     let result = 0;
     switch(operator) {
@@ -13,34 +25,43 @@ function operate(operator, firstNum, secondNum ) {
         case '*' :
             result  = ((a, b) => {return a*b;})(firstNum, secondNum);
             break;
-        case '/' :
+        case '÷' :
             result  = ((a, b) => {return a/b;})(firstNum, secondNum);
             break;
     }
+    console.log("firstNum:"+firstNum);
+    console.log("secondNum:"+secondNum);
+    console.log("result:"+result);
     return result;
 }
 
-function setOperator(operator){
-    operator = operator;
-    firstNum = document.getElementById("display").innerHTML;
-    document.getElementById("display").innerHTML = 0;
+function setOperator(op){
+    operator = op;
+    firstNum = getDisplay();
+    printDisplay(0);
 }
 
 function enterNumber(number){
-    if(firstNum){
-        document.getElementById("display").innerHTML += number;
+    if((number == 0) && (getDisplay() == 0)){
+        return;
     }
     else{
-        document.getElementById("display").innerHTML += number;
+        if(getDisplay() == 0){
+            printDisplay(number);
+        }
+        else{
+        printDisplay((getDisplay())+number);
+        }
     }
     console.log("enterNumber:"+number);
 }
 
 function readyCheck(){
+    console.log("operator:"+operator+"firstNum:"+firstNum+"secondNum:"+secondNum);
     let check = false;
     if(operator && firstNum && secondNum){
-        if(secondNum==0){
-            document.getElementById("display").innerHTML = "Sorry, you can't divide by zero."
+        if((secondNum==0) && (operator == "÷")){
+            printDisplay("Sorry, you can't divide by zero.")
         }
         else{
             check = true;
@@ -57,33 +78,47 @@ function readyCheck(){
 
 [...document.getElementsByClassName('operators')].forEach(operator => {
     operator.addEventListener('click', function() {
+        console.log(operator.value);
         setOperator(operator.value);
     })
 });
 
 document.getElementById("equals").addEventListener('click', function(){
-    if(secondNum){
+    if(firstNum){
+        secondNum = getDisplay();
         if(readyCheck()){
             let result = operate(operator, firstNum, secondNum);
-            document.getElementById("display").innerHTML = result;
+            printDisplay(result);
             firstNum = result;
         }
         else{
-            alert("Sorry, you need")
+            alert("operator:"+operator+"firstNum:"+firstNum+"secondNum:"+secondNum);
         }
     }
     else{
-        secondNum = document.getElementById("display").value;
+        secondNum = getDisplay();
         if(readyCheck()){
             let result = operate(operator, firstNum, secondNum);
-            document.getElementById("display").innerHTML = result;
+            printDisplay(result);
             firstNum = result;
         }
     }
-})
+});
 
+document.getElementById("clear").addEventListener('click', function(){
+    firstNum = false;
+    secondNum = false;
+    operator = false;
+    printDisplay(0);
+});
 
-let firstNum = false;
-let secondNum = false;
-let operator;
+document.getElementById("decimal").addEventListener('click', function(){
+    if(getDisplay().includes(".")){
+        return;
+    }
+    else{
+        printDisplay((getDisplay())+this.value);
+    }
+});
+
 console.log(operate("/", 2, 0));
